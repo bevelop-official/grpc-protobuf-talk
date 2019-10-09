5265769984bb9be09f189db4e81c5d2e60c3607c
maxsommer <max.sommer@bevelop.de>
2019-10-09 09:22:46 +0200

    Fixes automated startup

--- server/author-service/run.sh
+++ server/author-service/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /app
+cd /app/server/author-service
 if [ ! -d "node_modules" ]; then
   echo "Installing node_modules"
   npm install
 fi
 echo "Running author-service"
-ts-node-dev ./server.ts
+npm run start
 tail -f /dev/null
